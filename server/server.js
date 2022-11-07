a739ff015d3f83738775dcd1828577cc87bec3e3
Andrew Liu <aliu2@ufl.edu>
2022-11-07 10:47:07 -0500

    Set up basic server

---
+++ server/server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+require("dotenv").config({path: ".config/env"});
+const port = process.env.port || 5000;
+app.use(cors());
+app.use(express.json());
+app.use(require("./routes/record"));
+
+// driver connection
+const dbo = require("./db/conn");
+
+app.listen(port, () => {
+    // Perform a connection when the server starts
+    dbo.connectToServer(function (err) {
+        if (err) console.error(err);
+    });
+    console.log(`Server is running on port ${port}`);
+});
